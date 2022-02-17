54b001723d887990bb4bde4aebc5b03c079a089e
Zuzana Volfová <zuzka.volfova@gmail.com>
2022-02-17 19:52:56 +0100

    add odstavec, tlacitko, písmo v odstavci po najetí myši ztuční

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
 // tady je místo pro náš program
+function setBold(element) {
+    element.style.fontWeight = "bold"
+
+}
+
+function setNormal(element) {
+    element.style.fontWeight = "normal"
+
+}
+
+function odstavecBarva() {
+    let odstavec = document.querySelector('.odstavec')
+    odstavec.classList.toggle("cervenyText")
+}
